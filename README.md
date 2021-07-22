d6447f350307c195a4f530eef2660a66ccad2ed8
Matthew Hansen <MY_NAME@example.comlearntocode711@gmail.comlscleargit config --global user.email MY_NAME@example.comlearntocode711@gmail.comclear>
2021-07-22 14:22:57 +1000

    Money-Mover

---
+++ README.md
@@ -0,0 +1,38 @@
+# Money Mover
+
+## Description
+
+Money Mover is a simple application that allows you to tranfer money from one account to another and stores the transaction history for both accounts. All calculations are done on the back-end and there are no sessions/cookies/localstorage being used so progress is not stored. Both front-end and back-end are have validation and API testing has been implemented. 
+
+## Technologies
+
+- [Visual Studio Code](https://code.visualstudio.com/)
+- [NodeJS (includes npm)](https://nodejs.org/en/)
+- [React](https://reactjs.org/)
+- [reactstrap](https://reactstrap.github.io/) & [react-bootstrap](https://react-bootstrap.github.io/) for CSS framework
+
+
+## Launch
+
+- Open the terminal and use `cd` to navigate to your desktop or your preffered location to store this project.
+- Once there use the command `mkdir Money_Mover` to create a new folder entitled Beer_Buddies.
+- Now use the command `git clone https://github.com/King-cobra711/MoneyMover.git` to copy the project files from this github repository
+
+
+## Starting the App
+
+- In the terminal navigate to the money_mover folder and then type `npm i` to ensure all dependencies are installed and then `npm start`. Example:
+
+1. `cd /Users/KingCobra711/Desktop/Beer MoneyMover/client`
+1. `npm i` press enter
+2. `npm start` press enter
+
+Your React application should be running on http://localhost:3000 in your default browser.
+
+- then navigate to the server folder, ensure all dependencies are installed (`npm i`) and run `npm run devStart`. Example:
+
+1. `cd /Users/KingCobra711/Desktop/Beer MoneyMover/server`
+1. `npm i`
+2. `npm run devStart`
+
+Your Node server should be running on http://localhost:3001 in your default browser.
---
+++ server/index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const Cors = require("cors");
+const { body, validationResult } = require("express-validator");
+const app = express();
+
+app.use(
+  Cors({
+    origin: [
+      "http://localhost:3000",
+    ],
+    credentials: true,
+    allowedHeaders: ["Origin", "Content-Type", "Authorization"],
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.enable("trust proxy", true);
+
+let balanceA = 1000;
+let balanceB = 1000;
+// Routes
+
+app.get("/balances", (req, res) => {
+res.status(200).send({
+    Abalance: balanceA,
+    Bbalance: balanceB,
+})
+});
+
+app.post("/moneyToB", body("money", "must be $1 or higher").isFloat({ min:1}), body("money", "2 decimals max").isLength({max: 4}), (req,res) => {
+    const erroers = validationResult(req);
+    if (!erroers.isEmpty()){
+        return(
+            res.status(406).json({
+                error: erroers.array()
+            })
+        )
+    }else{
+        const Asent = req.body.money;
+        console.log(Asent);
+        balanceB = +balanceB + +Asent;
+        balanceA = balanceA -= Asent;
+        console.log(balanceA);
+        console.log(balanceB);
+        res.sendStatus(200);
+    }
+})
+app.post("/moneyToA", body("money", "must be $1 or higher").isFloat({ min:1}), body("money", "2 decimals max").isLength({max: 4}), (req,res) => {
+    const erroers = validationResult(req);
+    if (!erroers.isEmpty()){
+        return(
+            res.status(406).json({
+                error: erroers.array()
+            })
+        )
+    }else{
+        const Bsent = req.body.money;
+        console.log(Bsent);
+        balanceA = +balanceA + +Bsent;
+        balanceB = balanceB -= Bsent;
+        console.log(balanceA);
+        console.log(balanceB);
+        res.sendStatus(200);
+    }
+})
+
+// 
+
+module.exports = app;
